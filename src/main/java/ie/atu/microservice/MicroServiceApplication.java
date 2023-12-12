e2728c558bf1e7df037ee08313c75ab81fe194b2
Denis <G00403126@atu.ie>
2023-12-12 15:14:48 +0000

    Start

---
+++ src/main/java/ie/atu/microservice/MicroServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/microservice/MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
